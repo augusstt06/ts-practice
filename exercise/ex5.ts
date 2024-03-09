ceee0788b0294a88b3a26186d1e56e0ade5e1dfb
augusstt06 <augusstt06@gmail.com>
2024-03-09 16:57:19 +0900

    typescript exersice

---
+++ exercise/ex5.ts
@@ -0,0 +1,118 @@
+/*
+
+Intro:
+
+    Time to filter the data! In order to be flexible
+    we filter users using a number of criteria and
+    return only those matching all of the criteria.
+    We don't need Admins yet, we only filter Users.
+
+Exercise:
+
+    Without duplicating type structures, modify
+    filterUsers function definition so that we can
+    pass only those criteria which are needed,
+    and not the whole User information as it is
+    required now according to typing.
+
+Higher difficulty bonus exercise:
+
+    Exclude "type" from filter criteria.
+
+*/
+
+// My solve
+interface User {
+  type: "user";
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+interface Admin {
+  type: "admin";
+  name: string;
+  age: number;
+  role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    type: "user",
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    type: "admin",
+    name: "Jane Doe",
+    age: 32,
+    role: "Administrator",
+  },
+  {
+    type: "user",
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+  {
+    type: "admin",
+    name: "Bruce Willis",
+    age: 64,
+    role: "World saver",
+  },
+  {
+    type: "user",
+    name: "Wilson",
+    age: 23,
+    occupation: "Ball",
+  },
+  {
+    type: "admin",
+    name: "Agent Smith",
+    age: 23,
+    role: "Administrator",
+  },
+];
+
+export const isAdmin = (person: Person): person is Admin =>
+  person.type === "admin";
+export const isUser = (person: Person): person is User =>
+  person.type === "user";
+
+export function logPerson(person: Person) {
+  let additionalInformation = "";
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation;
+  }
+  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+// filterUser의 criteria paramter는 User 타입의 프로퍼티 중 무엇이 올지 알수 없다.
+// 하지만 적어도 User타입의 프로퍼티 중 하나가 온다는 것은 명확하므로, Utilty Type인 Partial을 사용하여 User 하위 프로퍼티들을 모두 parameter로 가질수 있게 해준다.
+export function filterUsers(
+  persons: Person[],
+  criteria: Partial<User>
+): User[] {
+  return persons.filter(isUser).filter((user) => {
+    const criteriaKeys = Object.keys(criteria) as (keyof User)[];
+    return criteriaKeys.every((fieldName) => {
+      return user[fieldName] === criteria[fieldName];
+    });
+  });
+}
+
+console.log("Users of age 23:");
+
+filterUsers(persons, {
+  age: 23,
+}).forEach(logPerson);
+
+// In case you are stuck:
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#predefined-conditional-types
